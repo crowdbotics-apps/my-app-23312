658caddf87475b4c479db1e33a7f0dfc983349ae
Crowdbotics <team@crowdbotics.com>
2020-12-11 02:19:03 +0000

    add package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'ArticleList4184293', human_name: 'Article List 4', access_route: 'ArticleList4184293'},
 { name: 'EmailAuth1184291', human_name: 'Email Auth 1', access_route: 'EmailAuth1184291', icon: 'envelope-o'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import ArticleList4184293Navigator from '../features/ArticleList4184293/navigator';
 import EmailAuth1184291Navigator from '../features/EmailAuth1184291/navigator';
 
 /**
@@ -14,6 +15,7 @@ import EmailAuth1184291Navigator from '../features/EmailAuth1184291/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+ArticleList4184293: { screen: ArticleList4184293Navigator },
 EmailAuth1184291: { screen: EmailAuth1184291Navigator },
 
     /** new navigators can be added here */
